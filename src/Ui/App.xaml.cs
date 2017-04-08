8de1d74ff9ffa461e7394065aabb66bb7837afb4
william-taylor <wi11berto@yahoo.co.uk>
2017-04-08 22:58:14 +0100

    cleaning up ui project

--- src/Ui/App.xaml.cs
+++ src/Ui/App.xaml.cs
@@ -1,7 +1,7 @@
 ﻿
 using System.Windows;
 
-namespace Trainer
+namespace Ui
 {
     public partial class App : Application
     {
--- src/Ui/EditorWindow.xaml.cs
+++ src/Ui/EditorWindow.xaml.cs
@@ -4,74 +4,49 @@ using System.Diagnostics;
 using System.Windows;
 using System.Xml;
 
-namespace Trainer
+namespace Ui
 {
     public partial class EditorWindow : Window
     {
+        private const string AboutText = "This is a simple simulation toolkit for RBF's & MLP's for the GameAI module";
+
         public EditorWindow()
         {
             InitializeComponent();
-            SetupDefaultValues();
-            SetupComboBox();
-        }
 
-        private void SetupDefaultValues()
-        {
-            MLP_ErrorThreshold.Text = "0.01";
-            MLP_LearningRate.Text = "0.1";
-            MLP_Epochs.Text = "500";
+            MLPErrorThreshold.Text = "0.01";
+            MLPLearningRate.Text = "0.1";
+            MLPEpochs.Text = "500";
 
-            RBF_ErrorThreshold.Text = "0.01";
-            RBF_LearningRate.Text = "0.1";
-            RBF_Epochs.Text = "500";
-        }
+            RBFErrorThreshold.Text = "0.01";
+            RBFLearningRate.Text = "0.1";
+            RBFEpochs.Text = "500";
 
-        private void SetupComboBox()
-        {
-            List<string> noiseValues = new List<string>();
-            noiseValues.Add("0.20");
-            noiseValues.Add("0.10");
-            noiseValues.Add("0.05");
-
-            NoiseComboBox.ItemsSource = noiseValues;
             NoiseComboBox.SelectedIndex = 2;
+            NoiseComboBox.ItemsSource = new List<string>() { "0.20", "0.10", "0.05" };
 
-            List<string> trainingSetSize = new List<string>();
-            trainingSetSize.Add("5");
-            trainingSetSize.Add("10");
-            trainingSetSize.Add("20");
-
-            TrainingComboBox.ItemsSource = trainingSetSize;
             TrainingComboBox.SelectedIndex = 1;
+            TrainingComboBox.ItemsSource = new List<string>() { "5", "10", "20" };
         }
-
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            if(sender == Exit)
-            {
+            if (sender == Exit)
                 Application.Current.Shutdown();
-            }
-
-            if(sender == About)
-            {
-                MessageBox.Show("This is a simple simulation toolkit for RBF's & MLP's for the GameAI module", "About ", MessageBoxButton.OK);
-            }
-            
-            if(sender == Run)
-            {
+            else if (sender == About)
+                MessageBox.Show(AboutText, "About", MessageBoxButton.OK, MessageBoxImage.Information);
+            else if (sender == Run)
                 RunSimulator();
-            }
         }
 
         private bool TestInputValues(params string[] arguments)
         {
-            double dummy;
-            int passes = 0;
+            var passes = 0;
+            var val = 0.0;
 
-            foreach(string str in arguments)
+            foreach (string str in arguments)
             {
-                if (double.TryParse(str, out dummy))
+                if (double.TryParse(str, out val))
                 {
                     ++passes;
                 }
@@ -82,53 +57,40 @@ namespace Trainer
 
         private void RunSimulator()
         {
-            // Test to make sure all values in the input boxes are correct
-            if (TestInputValues(MLP_ErrorThreshold.Text, MLP_LearningRate.Text, MLP_Epochs.Text))
+            if (TestInputValues(MLPErrorThreshold.Text, MLPLearningRate.Text, MLPEpochs.Text))
             {
-                // Test again to make sure all values in the input boxes are correct
-                if (TestInputValues(RBF_ErrorThreshold.Text, RBF_LearningRate.Text, RBF_Epochs.Text))
+                if (TestInputValues(RBFErrorThreshold.Text, RBFLearningRate.Text, RBFEpochs.Text))
                 {
                     try
                     {
-                        // Then alter the XML file which will be read by the simulator
-                        XmlDocument settings = new XmlDocument();
+                        var settings = new XmlDocument();
                         settings.Load(@"settings.xml");
-
-                        // Update the training set values and the noise values in the xml file
+                        settings.SelectSingleNode("/Settings/SettingsMLP/ErrorThreshold").InnerText = MLPErrorThreshold.Text;
+                        settings.SelectSingleNode("/Settings/SettingsMLP/LearningRate").InnerText = MLPLearningRate.Text;
+                        settings.SelectSingleNode("/Settings/SettingsMLP/Iterations").InnerText = MLPEpochs.Text;
+                        settings.SelectSingleNode("/Settings/SettingsRBF/ErrorThreshold").InnerText = RBFErrorThreshold.Text;
+                        settings.SelectSingleNode("/Settings/SettingsRBF/LearningRate").InnerText = RBFLearningRate.Text;
+                        settings.SelectSingleNode("/Settings/SettingsRBF/Iterations").InnerText = RBFEpochs.Text;
                         settings.SelectSingleNode("/Settings/TrainingSet").InnerText = TrainingComboBox.Text;
                         settings.SelectSingleNode("/Settings/Noise").InnerText = NoiseComboBox.Text;
-
-                        // Then update the setting values contained in SettingsMLP xml node
-                        settings.SelectSingleNode("/Settings/SettingsMLP/ErrorThreshold").InnerText = MLP_ErrorThreshold.Text;
-                        settings.SelectSingleNode("/Settings/SettingsMLP/LearningRate").InnerText = MLP_LearningRate.Text;
-                        settings.SelectSingleNode("/Settings/SettingsMLP/Iterations").InnerText = MLP_Epochs.Text;
-
-                        // And last update the setting values contained in SettingsRBF xml node
-                        settings.SelectSingleNode("/Settings/SettingsRBF/ErrorThreshold").InnerText = RBF_ErrorThreshold.Text;
-                        settings.SelectSingleNode("/Settings/SettingsRBF/LearningRate").InnerText = RBF_LearningRate.Text;
-                        settings.SelectSingleNode("/Settings/SettingsRBF/Iterations").InnerText = RBF_Epochs.Text;
-
-                        // The save the updated file & start the simulator
                         settings.Save(@"settings.xml");
+
                         Process.Start("Network.exe");
                     }
                     catch (Exception e)
                     {
                         MessageBox.Show(e.Message);
                     }
-                   
                 }
                 else
                 {
-                    // on fail say that the paramets in the RBF boxes are incorrect
                     MessageBox.Show("RBF Input Boxes do not contain valid values");
                 }
             }
             else
             {
-                // on fail say that the paramets in the MLP boxes are incorrect
                 MessageBox.Show("MLP Input Boxes do not contain valid values");
             }
-        }     
+        }
     }
 }
